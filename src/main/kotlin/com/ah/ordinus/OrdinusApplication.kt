47c4637dbc0b9ab210431867b73891beb3989491
anthon.havang <anthon.havang@sj.se>
2026-03-06 08:05:33 +0100

    Initial Ordinus commit

---
+++ src/main/kotlin/com/ah/ordinus/OrdinusApplication.kt
@@ -0,0 +1,11 @@
+package com.ah.ordinus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrdinusApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrdinusApplication>(*args)
+}
---
+++ src/test/kotlin/com/ah/ordinus/TestOrdinusApplication.kt
@@ -0,0 +1,9 @@
+package com.ah.ordinus
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.with
+
+
+fun main(args: Array<String>) {
+	fromApplication<OrdinusApplication>().with(TestcontainersConfiguration::class).run(*args)
+}
