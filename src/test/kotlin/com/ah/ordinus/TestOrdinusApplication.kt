c212c03cc2c3f4a0a3220f7ce1e9b0665ce7806a
Anthon Haväng <anthon.havang@sj.se>
2026-03-09 22:04:53 +0100

    add ktlint to workflow

--- src/test/kotlin/com/ah/ordinus/TestOrdinusApplication.kt
+++ src/test/kotlin/com/ah/ordinus/TestOrdinusApplication.kt
@@ -3,7 +3,6 @@ package com.ah.ordinus
 import org.springframework.boot.fromApplication
 import org.springframework.boot.with
 
-
 fun main(args: Array<String>) {
-	fromApplication<OrdinusApplication>().with(TestcontainersConfiguration::class).run(*args)
+    fromApplication<OrdinusApplication>().with(TestcontainersConfiguration::class).run(*args)
 }
