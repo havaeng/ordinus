71f9a36e1c70a3be90df0b4ae277a53479cb3cc2
Anthon Haväng <anthon.havang@sj.se>
2026-07-15 23:25:25 +0200

    disable blob seeding in tests by setting app.blob.seed.enabled to false

--- src/test/kotlin/com/ah/ordinus/TestcontainersConfiguration.kt
+++ src/test/kotlin/com/ah/ordinus/TestcontainersConfiguration.kt
@@ -3,6 +3,8 @@ package com.ah.ordinus
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.postgresql.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 
@@ -11,4 +13,12 @@ class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     fun postgresContainer(): PostgreSQLContainer = PostgreSQLContainer(DockerImageName.parse("postgres:latest"))
+
+    companion object {
+        @JvmStatic
+        @DynamicPropertySource
+        fun testProperties(registry: DynamicPropertyRegistry) {
+            registry.add("app.blob.seed.enabled") { false }
+        }
+    }
 }
